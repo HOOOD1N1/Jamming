73f7c161c9c3d529f1c374fe7fb319091520c6db
HOOOD1N1 <44617359+HOOOD1N1@users.noreply.github.com>
2021-02-23 10:33:24 +0200

    SearchBar state change sans fetch

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './SearchBar.css';
 export default class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.state={
+            term: ''
+        }
+    }
+    search(){
+        this.props.onSearch(this.state.term);
+    }
+    handleTermChange(event){
+        this.setState({term: event.target.value});
+    }
     render(){
         return (
         <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange}/>
         <button class="SearchButton">SEARCH</button>
         </div>
         );
